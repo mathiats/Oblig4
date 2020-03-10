e0a749cd0717a3df85dc9072b59dabdde0cf222e
mathiats <61471729+mathiats@users.noreply.github.com>
2020-03-10 10:35:20 +0100

    Update Legesystem.java

--- Legesystem.java
+++ Legesystem.java
@@ -5,7 +5,22 @@ class Legesystem{
   private Lenkeliste<Pasient> pasientListe;
 
   public void lesFraFil(String filnavn){
-    
+         String linje = fil.
+            String[] biter= fil.split(",");
+            if biter[0].contains("Pasient"){
+                fil.nextLine;
+                while(!(biter[0].contains("#")){
+                    String navn = biter[0];
+                    String fnr = biter[1];
+                    Pasient pasient= new Pasient(navn,fnr);
+                    pasientListe.leggTil(pasient);
+                    fil.nextLine;
+                }
+                if biter[0].contains("Pasient"){
+                fil.nextLine;
+
+                if biter[0].contains("Pasient"){
+                fil.nextLine;
 
  }
 
