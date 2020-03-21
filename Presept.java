46b87f8124243d6f243e894b3b96fa638c1a64ea
MaxDiggy <61869358+MaxDiggy@users.noreply.github.com>
2020-03-21 20:22:33 +0100

    Update Presept.java

--- Presept.java
+++ Presept.java
@@ -1,7 +1,7 @@
 class Presept extends HvitResept {
 
   public Presept(Legemiddel l,Lege u,Pasient p){
-    super(l,u,p,3); //Kaller på super sin konstruktør, og sender inn 3 som reit.
+    super(l,u,p,3); //Kaller paa super sin konstruktor, og sender inn 3 som reit.
 
   }
 
@@ -9,7 +9,7 @@ class Presept extends HvitResept {
 
   @Override //Overskriver prisAaBetale metoden.
   public double prisAaBetale(){
-    if (legemiddel.hentPris() > 108){ //Bruker en if-sjekk for å få riktig pris.
+    if (legemiddel.hentPris() > 108){ //Bruker en if-sjekk for aa faa riktig pris.
          return (legemiddel.hentPris() - 108);
         }
     return 0; // De skal ikke betale mindre enn 0.
